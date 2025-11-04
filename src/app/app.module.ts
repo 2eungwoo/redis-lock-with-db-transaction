e0053dab8bf128969fc708cf756c9dd4a9291bd9
swlee <swo98@ajou.ac.kr>
2025-11-04 22:21:55 +0900

    files: app -> /app 디렉토리 이동

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LockModule } from './lock/lock.module';
+import { LockModule } from '../lock/lock.module';
 
 @Module({
   imports: [LockModule],
