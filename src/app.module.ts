3140742ade7c93dc681695de3a5730139ab8d499
swlee <swo98@ajou.ac.kr>
2025-11-04 22:20:49 +0900

    add: lock module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LockModule } from './lock/lock.module';
 
 @Module({
-  imports: [],
+  imports: [LockModule],
   controllers: [AppController],
   providers: [AppService],
 })
