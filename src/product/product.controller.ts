817fb2c3ec8d6356ce29fa5534ab1bab0a1b7da3
swlee <swo98@naver.com>
2025-11-05 16:19:20 +0900

    add: dto랑 controller 추가

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { DeductStockDto } from './dto/deduct-stock.dto';
+import { Product } from './product.entity';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productService.createProduct(
+      createProductDto.name,
+      createProductDto.stock,
+    );
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Product> {
+    return this.productService.getProduct(id);
+  }
+
+  @Post(':id/deduct')
+  deductStock(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() deductStockDto: DeductStockDto,
+  ): Promise<Product> {
+    return this.productService.deductStock(id, deductStockDto.quantity);
+  }
 }
