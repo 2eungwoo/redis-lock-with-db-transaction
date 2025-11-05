6207147dff2981d20b1093cb2fa46261dcb23b56
swlee <swo98@naver.com>
2025-11-05 15:46:12 +0900

    docker: app 3개 띄우고 단일rdb,redis 바라보게 컨테이너 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LockModule } from '../lock/lock.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [LockModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // 개발 환경에서만 사용
+    }),
+    LockModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
