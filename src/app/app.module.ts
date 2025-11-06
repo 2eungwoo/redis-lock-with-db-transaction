39de10808dc69e1e1a0693f4bf81596600598029
swlee <swo98@naver.com>
2025-11-06 19:03:57 +0900

    fix: typeorm-transactional 사용을 위해서 datasource를 명시적으로 지정해줘야한다고 함

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,34 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisModule } from '../redis/redis.module';
 import { ProductModule } from '../product/product.module';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME || 'user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'test',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // 개발 환경에서만 사용
+};
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME || 'user',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_DATABASE || 'test',
-      autoLoadEntities: true,
-      synchronize: true, // 개발 환경에서만 사용
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return dataSourceOptions;
+      },
+      dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+        return Promise.resolve(
+          addTransactionalDataSource(new DataSource(options)),
+        );
+      },
     }),
     RedisModule,
     ProductModule,
